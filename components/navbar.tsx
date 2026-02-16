27d119039bad8f7d563cd3f12fcaf55ba2dd5df6
Alejandro Hernández <alejandro1998hernandez@gmail.com>
2026-02-16 18:56:49 +0100

    refactor: clean up imports in navbar, default layout, and index page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,6 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { Button } from "@heroui/button";
 import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
 import { Input } from "@heroui/input";
@@ -17,14 +16,7 @@ import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
+import { SearchIcon, Logo } from "@/components/icons";
 
 export const Navbar = () => {
   const searchInput = (
@@ -49,7 +41,7 @@ export const Navbar = () => {
   );
 
   return (
-    <HeroUINavbar maxWidth="100vw" position="sticky" className="bg-default-100">
+    <HeroUINavbar className="bg-default-100" maxWidth="full" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
